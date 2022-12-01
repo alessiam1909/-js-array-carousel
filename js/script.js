d2275b01ca7d7c0e2c414b348794a72a12a21cf8
alessiam1909 <ale.menon.vr@gmail.com>
2022-12-01 20:52:07 +0100

    genera l'immagine successiva al click

--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ let itemsContent = '';
 for(let i = 0; i < imagesArray.length; i++){
     itemsContent += `<div class="item">
         <img src="./img/${imagesArray[i]}">
-    </div>`
+    </div>`;
+    
 }
 
 //inseriamo le immagini nel div che le deve contenere
@@ -29,6 +30,7 @@ let itemActive = 0;
 
 items[itemActive].classList.add('active');
 
+
 //rendo attivo anche il primo cerchio di navigazione
 
 const circles = document.getElementsByClassName('circle');
@@ -39,6 +41,14 @@ const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
 next.addEventListener('click', function(){
+    if(items[itemActive]){
+        itemActive++;
+        items[itemActive].classList.add('active');
+        console.log(items[itemActive]);
+    };
+    
+    
+
     //verifico l'elemento attivo (itemActive)
     //incremento il suo valore di 1
     //aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
@@ -46,6 +56,7 @@ next.addEventListener('click', function(){
 });
 
 prev.addEventListener('click', function(){
+
     //verifico l'elemento attivo (itemActive)
     //decremento il suo valore di 1
     //aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
